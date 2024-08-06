1a3a22caf77ab74c2c08af59e437006aef1a8815
tang <1363269065@qq.com>
2024-08-06 13:07:33 +0800

    first commit

---
+++ myblog/src/main/java/com/tzy/MyblogApplication.java
@@ -0,0 +1,14 @@
+package com.tzy;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class MyblogApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
